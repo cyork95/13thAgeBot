555829e09d4d3acae567ff373b652c989b7ffedc
Cody York <yorkc36@gmail.com>
2021-09-28 13:29:34 +0000

    Add Encouraging Responses

---
+++ commands/apis/inspire.py
@@ -0,0 +1,23 @@
+import requests
+import json
+from replit import db
+
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + " -" + json_data[0]['a']
+  return(quote)
+
+def update_encouragements(encouraging_message):
+  if "encouragements" in db.keys():
+    encouragements = db["encouragements"]
+    encouragements.append(encouraging_message)
+    db["encouragements"] = encouragements
+  else:
+    db["encouragements"] = [encouraging_message]
+
+def delete_encouragements(index):
+  encouragements = db["encouragements"]
+  if len(encouragements) > index:
+    del encouragements[index]
+  db["encouragements"] = encouragements
--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import os
 import discord
+import random as random
+from commands.apis import inspire
+from replit import db
 
 client = discord.Client()
+
+sad_words = ["sad", "depressed", "unhappy", "angry", "miserable", "depressing"]
+starter_encouragements = [
+  "Cheer Up!", 
+  "Hang in there.", 
+  "You are a great person! (or bot)"
+  ]
 
 @client.event
 async def on_ready():
@@ -12,7 +22,37 @@ async def on_ready():
 async def on_message(message):
   if message.author == client.user:
     return
-  if message.content.startswith('hello') or message.content.startswith('hi'):
+
+  msg = message.content
+
+  if msg.startswith('hello') or message.content.startswith('hi'):
     await message.channel.send('Hello!')
+
+  if msg.startswith('-inspire'):
+    quote = inspire.get_quote()
+    await message.channel.send(quote)
+
+  if any(word in msg for word in sad_words):
+    options = starter_encouragements
+    if "encourgements" in db.keys():
+      options = options + db['encourgements']
+    await message.channel.send(random.choice(options))
+
+  if msg.startswith("-new_encourage"):
+    encouraging_message = msg.split("-new_encourage ",1)[1]
+    inspire.update_encouragements(encouraging_message)
+    await message.channel.send("New encouraging message added!")
+
+  if msg.startswith("-del_encourage"):
+    if "encouragements" in db.keys():
+      index = int(msg.split("-del_encourage ",1)[1])
+      inspire.delete_encouragements(index)
+    await message.channel.send("Encouraging message deleted!")
+
+  if msg.startswith("-lst_encourage"):
+    encouragments = []
+    if "encouragements" in db.keys():
+      encouragments = db["enncouragments"]
+    await message.channel.send(encouragments)
 
 client.run(os.environ['TOKEN'])
